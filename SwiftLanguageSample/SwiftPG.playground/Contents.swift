cecbf1691846dfbed98511d2718ed45c192e230f
chenkai <kaidocumentgroup@gmail.com>
2016-06-20 23:50:22 +0800

    basic sample 03

--- SwiftLanguageSample/SwiftPG.playground/Contents.swift
+++ SwiftLanguageSample/SwiftPG.playground/Contents.swift
@@ -268,6 +268,252 @@ hasAnyMatch(list: numbers, condition: lessThanTen)
 
 //闭包尚未查看.
 
+//对象和类
+
+class customerRecord {
+    var customername = "green lin"
+    func combineAddress(customername:String) -> String {
+        return "customer name \(customername) is combine operatore is done"
+    }
+}
+
+var firstcustomer = customerRecord()
+firstcustomer.customername = "chenkai" //有没有私有变量一说？
+firstcustomer.combineAddress(customername: "deyu")
+
+class collectionRecord{
+    var clientversion = "1.0"
+    init(clientversion:String) {//构造函数
+        self.clientversion = clientversion
+    }
+    
+    deinit {//析构函数
+        
+    }
+    
+    func uploaddata() -> String {
+        return "device client version \(clientversion)"
+    }
+}
+
+var collectiondef = collectionRecord.init(clientversion: "2.36")
+var colvalue = collectiondef.uploaddata()
+
+//子类(继承)
+
+class childdefine : collectionRecord{
+    var sideLength:Double
+    init(sideLength:Double,version:String) {
+        self.sideLength = sideLength
+        super.init(clientversion:version )
+        clientversion = "V5.2"
+    }
+    
+    func area() -> String {
+        return "device is aleady fresh \(clientversion)"
+    }
+    
+    override func uploaddata() -> String {
+        return "child class override method"
+    }
+}
+
+//练习子类
+
+class NamedShape {
+    var numberOfSides:Int = 0
+    var name:String
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "a shape with \(numberOfSides) sides."
+    }
+}
+
+class Circle:NamedShape {
+    var circle:Double
+    init(circleWidth:Double,name:String) {
+        self.circle = circleWidth
+        super.init(name: name)
+    }
+    
+    func area() -> Double {
+        return self.circle * 2
+    }
+    
+    override func simpleDescription() -> String {
+        return "Circle Simple Description Document"
+    }
+}
+
+var singleCircle = Circle.init(circleWidth: 20.5, name: "Circle Shape")
+singleCircle .area()
+
+//问题类中属性是否有私有private 或者公开public区别
+
+class EquilateraTriangle : NamedShape{
+    var sideLength:Double = 0.0
+    init(sideLength:Double,name:String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter:Double {
+        get{
+            return 3.0 * sideLength
+        }
+        set{
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "an simple document has been descri"
+    }
+}
+
+var target = EquilateraTriangle.init(sideLength: 3.1, name: "ample")
+var single = EquilateraTriangle(sideLength: 34,name: "d")
+print(target.perimeter)
+target.perimeter = 10
+print(target.perimeter)
+
+//newValue 类似关键字 代表新值
+
+//WillSet 和 DidSet
+//在Swift 3.0 貌似这个属性没有了
+class TriangleAndSquare {
+//    var triangle:EquilateraTriangle {
+//      willSet{
+    
+//       }
+//    }
+}
+
+//方法和函数的参数别名用法
+class counter {
+    var count:Int = 0
+    func incrementBy(amount:Int,numberOfTimes times:Int)  {
+        count += amount * times
+    }
+}
+
+var counterSingle = counter()
+counterSingle.incrementBy(amount: 3, numberOfTimes: 8)
+
+
+//变量可选值 Swift 3.0 中不可用了吗？
+//let sideLength = counterSingle?.count
+
+
+//枚举和结构体
+
+enum Rank:Int{
+    case Ace = 1 //Int类型 字符串或者浮点数
+    case Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten
+    case Jack,Queen,King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .King:
+            return "king"
+        case .Queen:
+            return "queen"
+        default:
+            return String(self .rawValue)
+        }
+    }
+}
+
+let ace = Rank.Ace
+let aceRawValue = ace.rawValue
+
+//Rawvalue 在原始值和枚举值之间切换
+if let convertedRank = Rank(rawValue:3){
+    let threeDesc = convertedRank.simpleDescription()
+}
+
+//枚举和结构体还需要在看
+
+//协议和扩展
+
+protocol ExampleProtocol {
+    var simpleDescription: String {get}
+    mutating func adjust()//Mutating 关键字做什么用的？
+}
+
+class SimpleClass:ExampleProtocol {
+    var simpleDescription: String = "a very simple class"
+    var anotherProperty:Int = 6915
+    func adjust() {
+         simpleDescription += "now 100% adjusted"
+    }
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure : ExampleProtocol {
+    var simpleDescription: String = "a simple Structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+
+//enum RankType:ExampleProtocol {
+//    var simpleDescription: String = " a simple enum type"
+//    mutating func adjust() {
+//        simpleDescription += " (enum type)"
+//    }
+//}
+
+//mutating关键字是用来标记一个会修改结构体的方法 
+//SimpleClass 不需要标记任何方法因为类中的方法经常会修改类
+
+extension Int:ExampleProtocol {
+    var simpleDescription:String{
+        return "The Number \( self )"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+
+7.simpleDescription
+//7.adjust() //为什么不能调用adjust方法？ 因为7是一个常量
+//编译错误：cannot use mutating member on immutable value: literals are not mutable
+7.simpleDescription
+
+
+//采用Var 调用adjust方法成功
+var protocolValue : ExampleProtocol = 12
+protocolValue.simpleDescription
+protocolValue.adjust()
+protocolValue.simpleDescription
+
+//不可抗拒的外力导致这块没看清楚 稍后在看.
+
+//泛型
+//var ItemType = "done"
+//func repeat<ItemType> (item:ItemType,times:Int) -> [ItemType]{
+//    var result = [ ItemType ]()
+//    for i in  0 ..<times {
+//        result.append(item)
+//    }
+//    return result
+//}
+
+
 
 
 
